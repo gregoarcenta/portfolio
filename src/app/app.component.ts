f60c8833e268a56f19d0eef850d9bd9b2d008cdc
Gregory Arcentales <gregoarcenta@gmail.com>
2024-12-12 20:11:45 -0500

    config ngx translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '@/components/navbar/navbar.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <div>
+    <app-navbar />
+    <main class="container mx-auto mt-4">
       <router-outlet />
-    </div>
+    </main>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly translate: TranslateService) {
+    this.translate.addLangs(['en', 'es']);
+    const browserLang = this.translate.getBrowserLang();
+    if (browserLang === 'es') {
+      this.translate.use('es');
+    } else {
+      this.translate.use('en');
+    }
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient,
+) => new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideTranslateService({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+    }),
+  ],
 };
