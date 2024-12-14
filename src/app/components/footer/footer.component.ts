0534d7956b5d4c53182f2c91190642dfeda75e73
Gregory Arcentales <gregoarcenta@gmail.com>
2024-12-14 14:36:07 -0500

    Toastify added

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import Toastify from 'toastify-js';
 
 @Component({
   selector: 'app-footer',
@@ -15,7 +16,19 @@ export class FooterComponent {
   copyEmail() {
     const email = 'gregoarcenta@gmail.com';
     navigator.clipboard.writeText(email).then(() => {
-      alert('Correo copiado al portapapeles');
+      Toastify({
+        text: '¡Correo electrónico copiado!',
+        duration: 3000,
+        gravity: 'bottom',
+        position: 'center',
+        stopOnFocus: true,
+        className: 'flex flex-row-reverse text-gray-800 text-lg',
+        style: {
+          background: '#ffffff',
+          borderRadius: '24px',
+          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+        },
+      }).showToast();
     });
   }
 }
