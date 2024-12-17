ee7b9f5bacd2cf3bf554df7d35bbeb2b468ff421
Gregory Arcentales <gregoarcenta@gmail.com>
2024-12-17 15:57:35 -0500

    Refactoring profile of home page

--- src/app/components/home/profile-content/profile-content.component.ts
+++ src/app/components/home/profile-content/profile-content.component.ts
@@ -1,15 +1,20 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile-content',
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage, RouterLink, TranslatePipe],
   templateUrl: './profile-content.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    class: 'flex flex-col xl:flex-row',
-  },
+  styles: `
+    .h1-color {
+      -webkit-background-clip: text !important;
+      -webkit-text-fill-color: transparent !important;
+      background: linear-gradient(135deg, #f2c511, #eeeeee);
+    }
+  `,
 })
 export class ProfileContentComponent {
   public imageProfile = signal('/assets/images/profile.jpg');
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LinksFooterComponent } from '@/components/home/links-footer/links-footer.component';
-import { AbilitiesComponent } from '@/components/home/abilities/abilities.component';
 import { ProfileContentComponent } from '@/components/home/profile-content/profile-content.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [LinksFooterComponent, AbilitiesComponent, ProfileContentComponent],
+  imports: [LinksFooterComponent, ProfileContentComponent],
   selector: 'app-home',
   templateUrl: './home.component.html',
   host: { class: 'block h-full p-5 pb-0' },
