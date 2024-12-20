f8811d7f712dbdfe18eb60a434adef70dcd72dc4
Gregory Arcentales <gregoarcenta@gmail.com>
2024-12-19 23:51:43 -0500

    added translate about toast copy email

--- src/app/components/home/links-footer/links-footer.component.ts
+++ src/app/components/home/links-footer/links-footer.component.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import copy from 'copy-to-clipboard';
 import Toastify from 'toastify-js';
 import { RouterLink } from '@angular/router';
-import { TranslatePipe } from '@ngx-translate/core';
+import { _, TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-links-footer',
@@ -12,31 +18,39 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class LinksFooterComponent {
   public email = signal<string>('gregoarcenta@gmail.com');
+  private readonly translate = inject(TranslateService);
 
   copyEmail() {
     const hasBeenCopy = copy(this.email());
     const btnSuccess = document.getElementById('success-icon')!;
     const defaultIcon = document.getElementById('default-icon')!;
+
     defaultIcon.classList.add('hidden');
     btnSuccess.classList.remove('hidden');
+
     setTimeout(() => {
       defaultIcon.classList.remove('hidden');
       btnSuccess.classList.add('hidden');
     }, 3000);
 
-    if (hasBeenCopy) {
-      Toastify({
-        text: '¡Correo electrónico copiado!',
-        duration: 3000,
-        gravity: 'bottom',
-        position: 'center',
-        stopOnFocus: true,
-        className: 'flex flex-row-reverse text-gray-800 text-lg rounded-lg',
-        style: {
-          background: '#ffffff',
-          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-        },
-      }).showToast();
-    }
+    this.translate
+      .get(_(`footer.emailCopied`))
+      .pipe(take(1))
+      .subscribe((message: string) => {
+        if (hasBeenCopy) {
+          Toastify({
+            text: message,
+            duration: 3000,
+            gravity: 'bottom',
+            position: 'center',
+            stopOnFocus: true,
+            className: 'flex flex-row-reverse text-gray-800 text-lg rounded-lg',
+            style: {
+              background: '#ffffff',
+              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+            },
+          }).showToast();
+        }
+      });
   }
 }
